376d543e197fcccef80c501ecfbf8af62b9f67dd
pushpajuluri <pushpa.juluri@gmail.com>
2017-05-04 08:31:35 +0530

    Update Quadratic2Test.java

--- src/test/java/weekendtest/Quadratic2Test.java
+++ src/test/java/weekendtest/Quadratic2Test.java
@@ -8,29 +8,17 @@ import org.junit.Test;
 public class Quadratic2Test {
 	@Test
 	public void test() {
-		Quadrantic2 q=new Quadrantic2();
+		Quad q=new Quad();
 		Quad p=q.solveBiquadratic(1.0,3.0,0.0);
 		double x1=p.getX1();
 		double x2=p.getX2();
 		double x3=p.getX3();
 		double x4=p.getX4();
-		assertEquals(0,x1,0);
-		assertEquals(0,x2,0);
-		assertEquals(3,x3,0);
-		assertEquals(-3,x4,0);
+		assertEquals(double.NaN,x1,0);
+		assertEquals(double.NaN,x2,0);
+		assertEquals(0,x3,0);
+		assertEquals(0,x4,0);
 	}
-		public void test1() {
-			Quadrantic2 q2=new Quadrantic2();
-			Quad q21=q2.solveBiquadratic(1, 5, 6);
-			double x5=q21.getX1();
-			double x6=q21.getX2();
-			double x7=q21.getX3();
-			double x8=q21.getX4();
-			assertEquals(0,x5,0);
-		    assertEquals(-3,x6,0);
-			assertEquals(3,x7,0);
-			assertEquals(-2,x8,0);
-		}
 		
 		
 	}
