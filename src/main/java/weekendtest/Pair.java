a48d47d98b8c091e6bf468dbc09ece82992cb5c9
pushpajuluri <pushpa.juluri@gmail.com>
2017-05-04 08:18:06 +0530

    Update Pair.java

--- src/main/java/weekendtest/Pair.java
+++ src/main/java/weekendtest/Pair.java
@@ -21,6 +21,19 @@ public class Pair implements Ipair {
 	public double getX() { return x1; }
 	public double getY() { return x2; }
 	
+	
+	public Pair solveQuadratic(double a, double b, double c) {
+		double discriminant = b*b - 4*a*c;
+		double x1=0;
+		double x2=0;
+		if (discriminant < 0)
+		
+			x1=(-b+Math.sqrt(discriminant))/(2*a);
+			x2=(-b-Math.sqrt(discriminant))/(2*a);
+			Pair p1=new Pair(x1,x2);
+			return p1;
+	}
+	
 }
 	
 		
